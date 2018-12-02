ae89fc72d3f19fdeccd73c97822c2decb9f5c8af
Lionel Damtew <damtew.lionel@gmail.com>
2018-12-02 19:43:13 +0100

    Add Possiblity to play video

---
+++ Assets/Scripts/CustomVideoPlayer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class CustomVideoPlayer : MonoBehaviour {
+
+    private VideoPlayer videoPlayerComponent;
+
+    private void Awake()
+    {
+        videoPlayerComponent = gameObject.GetComponent<VideoPlayer>();
+        if (videoPlayerComponent == null)
+        {
+            Debug.Log("Could not find VideoPlayer-Component");
+        }
+    }
+
+    private void OnEnable()
+    {
+        videoPlayerComponent.frame = 0;
+        videoPlayerComponent.Play();
+    }
+
+    private void OnDisable()
+    {
+        videoPlayerComponent.Pause();
+    }
+}

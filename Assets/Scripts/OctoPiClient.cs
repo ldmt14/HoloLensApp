b9556ebdb69808206820bb7fd5c765de2f803e37
Lionel Damtew <damtew.lionel@gmail.com>
2018-12-08 15:09:33 +0100

    Add Client and methods to display PrintInformation

---
+++ Assets/Scripts/OctoPiClient.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace OctoPi
+{
+    public delegate void JobInformationCallback(JobInformationResponse response);
+
+    public class OctoPiClient : MonoBehaviour
+    {
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        public static void GetJobInformation(JobInformationCallback callback)
+        {
+            JobInformationResponse result;
+            var json = @"{
+  ""job"": {
+    ""file"": {
+      ""name"": ""whistle_v2.gcode"",
+      ""origin"": ""local"",
+      ""size"": 1468987,
+      ""date"": 1378847754
+    },
+    ""estimatedPrintTime"": 8811,
+    ""filament"": {
+      ""length"": 810,
+      ""volume"": 5.36
+    }
+  },
+  ""progress"": {
+    ""completion"": 0.2298468264184775,
+    ""filepos"": 337942,
+    ""printTime"": 276,
+    ""printTimeLeft"": 912
+  }
+}";
+            result = JsonUtility.FromJson<JobInformationResponse>(json);
+            callback.Invoke(result);
+        }
+    }
+}
---
+++ Assets/Scripts/OctoPiObject.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace OctoPi
+{
+    public class OctoPiObject : InspectableObject
+    {
+        [SerializeField]
+        private Slider progressBar;
+        [SerializeField]
+        private Slider temperatureBar;
+        [SerializeField]
+        private Text fileNameText;
+
+        public override void Help()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        // Use this for initialization
+        void Start()
+        {
+            InvokeRepeating("UpdateUI", 2, 2);
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        private void UpdateUI()
+        {
+            OctoPiClient.GetJobInformation((response) =>
+            {
+                progressBar.value = response.progress.completion * progressBar.maxValue;
+                fileNameText.text = response.job.file.name;
+            });
+        }
+    }
+}

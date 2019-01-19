5f0fd91e44930728e0d77549b1a5cfea70c9f5e3
Lionel Damtew <damtew.lionel@gmail.com>
2019-01-19 15:06:45 +0100

    Add Vuforia

---
+++ Assets/Scripts/MainCameraSetup.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class MainCameraSetup : MonoBehaviour {
+
+#if UNITY_EDITOR
+
+    // Update is called once per frame
+    void Update()
+    {
+        var camera = GetComponent<Camera>();
+        foreach (var canvas in Resources.FindObjectsOfTypeAll<Canvas>())
+        {
+            canvas.renderMode = RenderMode.WorldSpace;
+            canvas.worldCamera = camera;
+        }
+    }
+#endif
+}

7df3c057e3e6012d4bce1e7c6b28b533f16bb41a
ldmt14 <damtew.lionel@gmail.com>
2019-01-21 13:50:49 +0100

    Add Navigation Buttons to Instructions

--- Assets/Scripts/InstructionObject.cs
+++ Assets/Scripts/InstructionObject.cs
@@ -2,16 +2,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class InstructionObject : InspectableObject {
 
     public InstructionStep[] stepList;
     private int currentIndex = 0;
+    [SerializeField]
+    private Button nextButton;
+    [SerializeField]
+    private Button backButton;
+    [SerializeField]
+    private Button closeButton;
 
-	// Use this for initialization
-	void Awake()
+    // Use this for initialization
+    void Awake()
     {   
         currentIndex = 0;
+        if (nextButton != null)
+        {
+            nextButton.onClick.AddListener(NextStep);
+        }
+        if (backButton != null)
+        {
+            backButton.onClick.AddListener(PreviousStep);
+        }
+        if (closeButton != null)
+        {
+            closeButton.onClick.AddListener(Close);
+        }
         DisplayCurrentStep();
 	}
 	
@@ -54,6 +73,14 @@ public class InstructionObject : InspectableObject {
         for (int i = 0; i < stepList.Length; i++)
         {
             stepList[i].Step.SetActive(i == currentIndex);
+        }
+        if (nextButton != null)
+        {
+            nextButton.gameObject.SetActive(currentIndex != stepList.Length - 1);
+        }
+        if (backButton != null)
+        {
+            backButton.gameObject.SetActive(currentIndex != 0);
         }
     }
 
